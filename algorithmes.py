56d2765c583fb0357a22d14fca2e11310f3c8bd3
Genevieve <genevieve.paul-hus@umontreal.ca>
2021-11-16 01:01:51 -0500

    Cleanup algorithmes.py

--- algorithmes.py
+++ algorithmes.py
@@ -1,13 +1,12 @@
 import numpy as np
 import warnings
-import adult
 
-from matplotlib import pyplot as plt
 from sklearn import neighbors
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import KernelPCA
 from sklearn.manifold import Isomap
-from sklearn_extra.cluster import KMedoids
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn_extra.cluster import KMedoids  # pip install scikit-learn-extra
 
 
 # Algorithmes
@@ -20,33 +19,19 @@ def knn(voisins, train_matrice, test_matrice, y_train, y_test):
     knn_train = clf.predict(train_matrice)
     knn_test = clf.predict(test_matrice)
 
-    print("Le score de knn est " + str(clf.score(test_matrice, y_test)))
-
-    # TODO ajouter algo qui verifie l'accuracy
+    print("Le rapport de précision pour knn est: \n" + classification_report(knn_test, y_test))
 
 
 # k-médoides
-def kmd(train_matrice, test_matrice):
+def kmd(train_matrice, test_matrice, y_train):
     warnings.filterwarnings("ignore")
     kmedoids_instance = KMedoids(n_clusters=4, metric='precomputed', random_state=0)
-    kmedoids_instance.fit(train_matrice, adult.y_train)
+    kmedoids_instance.fit(train_matrice, y_train)
 
     closest_clusters_train = kmedoids_instance.predict(train_matrice)
     closest_clusters_test = kmedoids_instance.predict(test_matrice)
 
     #TODO modifier ici-bas et ajouter algo qui verifie l'accuracy
-
-    # fig = plt.figure(figsize=(12, 6))
-    # train_ax = fig.add_subplot(121)
-    # test_ax = fig.add_subplot(122)
-    #
-    # for i in range(4):
-    #     train_cluster = adult.X_train[np.where(closest_clusters_train == i)[0]]
-    #     train_ax.scatter(train_cluster[:, 0], train_cluster[:, 1])
-    #
-    #     test_cluster = adult.X_test[np.where(closest_clusters_test == i)[0]]
-    #     train_ax.scatter(test_cluster[:, 0], test_cluster[:, 1])
-    # plt.show()
 
 
 # Isomap
@@ -57,20 +42,6 @@ def isomap(components, voisins, train_matrice, test_matrice):
 
     # TODO modifier ici-bas et ajouter algo qui verifie l'accuracy
 
-    # plt.show()
-    #
-    # fig = plt.figure(figsize=(12, 8))
-    # ax = fig.add_subplot(211)
-    # ax.set_title('Cosine Isomap sur circle')
-    # t = np.arange(len(isomap_trainset))
-    # ax.scatter(isomap_trainset, np.zeros_like(isomap_trainset), c= t)
-    #
-    # ax = fig.add_subplot(212)
-    # ax.set_title('Cosine Isomap sur infinity')
-    # t = np.arange(len(isomap_testset))
-    # ax.scatter(isomap_testset, np.zeros_like(isomap_testset), c= t)
-    # plt.show()
-
 
 # PCoA
 def pcoa(components, train_matrice, test_matrice):
@@ -79,18 +50,6 @@ def pcoa(components, train_matrice, test_matrice):
     pcoa_testset = pcoa.transform(-.5 * test_matrice ** 2)
 
     # TODO modifier ici-bas et ajouter algo qui verifie l'accuracy
-
-    # fig = plt.figure(figsize=(12, 8))
-    # ax = fig.add_subplot(211)
-    # ax.set_title('Cosine PCoA sur circle')
-    # t = np.arange(len(pcoa_trainset))
-    # ax.scatter(pcoa_trainset, np.zeros_like(pcoa_trainset), c=t)
-    #
-    # ax = fig.add_subplot(212)
-    # ax.set_title('Cosine PCoA sur infinity')
-    # t = np.arange(len(pcoa_testset))
-    # ax.scatter(pcoa_testset, np.zeros_like(pcoa_testset), c=t)
-    # plt.show()
 
 
 # Regroupement hiérarchique
@@ -109,18 +68,4 @@ def hierarchique(clusters, train_matrice, test_matrice):
     agglo_testset = agglomerative_clustering_predict(agglomerative_clustering, test_matrice)
 
     # TODO modifier ici-bas et ajouter algo qui verifie l'accuracy
-
-    # fig = plt.figure(figsize=(12, 6))
-    # circle_ax = fig.add_subplot(121)
-    # infinity_ax = fig.add_subplot(122)
-    # circle_ax.set_title('Cosine regroupement hiérarchique sur circle')
-    # infinity_ax.set_title('Cosine regroupement hiérarchique sur infinity')
-    #
-    # for i in range(5):
-    #     circle_cluster = adult.X_train[np.where(agglo_trainset == i)[0]]
-    #     circle_ax.scatter(circle_cluster[:, 0], circle_cluster[:, 1])
-    #
-    #     infinity_cluster = adult.X_test[np.where(agglo_testset == i)[0]]
-    #     infinity_ax.scatter(infinity_cluster[:, 0], infinity_cluster[:, 1])
-    # plt.show()
 
