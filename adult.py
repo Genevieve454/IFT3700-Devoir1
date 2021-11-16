4b367dcd6e21bd7e2a446f0b739d1e497a69a54e
Genevieve <genevieve.paul-hus@umontreal.ca>
2021-11-15 22:24:57 -0500

    Clean up de adult.py

--- adult.py
+++ adult.py
@@ -1,13 +1,11 @@
 import algorithmes
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-from sklearn import neighbors, preprocessing
 from sklearn.model_selection import train_test_split
 
 # Occupation classification. From physical (0) to intellectual (2)
-occupationClassif = {
+occupation_classification = {
     "Adm-clerical": 2,
     "Armed-Forces": 0,
     "Craft-repair": 1,
@@ -24,17 +22,26 @@ occupationClassif = {
     "Transport-moving": 1
 }
 
-maritalstatus_issingle = {
-"Divorced": 1,
-"Married-AF-spouse" : 0,
-"Married-civ-spouse" : 0,
-"Married-spouse-absent": 1,
-"Never-married": 1,
-"Separated": 1,
-"Widowed": 1
+# Marital status classification
+marital_status_classification = {
+    "Divorced": 1,
+    "Married-AF-spouse": 0,
+    "Married-civ-spouse": 0,
+    "Married-spouse-absent": 1,
+    "Never-married": 1,
+    "Separated": 1,
+    "Widowed": 1
 }
 
-relationship_ordering = {'Unmarried':0,'Wife':1,'Husband':2,'Not-in-family':3,'Own-child':4,'Other-relative':5}
+# Relationship classification
+relationship_classification = {
+    'Unmarried': 0,
+    'Wife': 1,
+    'Husband': 2,
+    'Not-in-family': 3,
+    'Own-child': 4,
+    'Other-relative': 5
+}
 
 
 def prep(person):
@@ -43,25 +50,25 @@ def prep(person):
                     ])  
 
     if person[1] != "?":
-        result = np.append(result, occupationClassif[person[1]] / 2)  # occupation, range from 0 to 2
+        result = np.append(result, occupation_classification[person[1]] / 2)  # occupation, range from 0 to 2
     else:
         result = np.append(result, 0.5)
 
-    result = np.append(result, maritalstatus_issingle[person[2]])
-    result = np.append(result, relationship_ordering[person[3]]/5)
+    result = np.append(result, marital_status_classification[person[2]])
+    result = np.append(result, relationship_classification[person[3]]/5)
 
     print(abs(result))
     return abs(result)
 
 
-def fonction_similarite_adult(x,y):
+def fonction_similarite_adult(x, y):
     delta = 1 - abs(np.apply_along_axis(prep, 2, x) - np.apply_along_axis(prep, 2, y))
-    return np.average(delta,2)
+    return np.average(delta, 2)
 
 
 # Fonction de dissimilarité
 def fonction_dissimilarite_adult(x, y):
-    return 1 - fonction_similarite_adult(x,y)
+    return 1 - fonction_similarite_adult(x, y)
 
 
 # Fonction pour obtenir une matrice de similarite
@@ -72,19 +79,12 @@ def get_fonction_dissimilarite_adult_matrix(X, Y=None):
 
 # Jeu de données
 df = pd.read_csv("data/adult.csv")
-cts_columns = ['age', 'occupation', 'marital-status','relationship']
-
+cts_columns = ['age', 'occupation', 'marital-status', 'relationship']
 X = df[cts_columns]
-
-# Transforme le gender en 0 et 1
-le = preprocessing.LabelEncoder()
-le.fit(df['gender'])
-df['gender'] = le.transform(df['gender'])
 y = df['gender']
 
 # Division des données
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.3, test_size=0.2, stratify=y, random_state=0)
-
 X_train = pd.DataFrame(X_train).to_numpy()
 X_test = pd.DataFrame(X_test).to_numpy()
 
@@ -94,8 +94,8 @@ test_matrice_dissimilarite = get_fonction_dissimilarite_adult_matrix(X_test, X_t
 
 
 def run_adult():
-    # algorithmes.kmd([0, 1, 2], train_matrice_dissimilarite, test_matrice_dissimilarite)
-    # algorithmes.isomap(1, 2, train_matrice_dissimilarite, test_matrice_dissimilarite)
-    # algorithmes.pcoa(1, train_matrice_dissimilarite, test_matrice_dissimilarite)
-    # algorithmes.hierarchique(5, train_matrice_dissimilarite, test_matrice_dissimilarite)
+    algorithmes.kmd([0, 1, 2], train_matrice_dissimilarite, test_matrice_dissimilarite)
+    algorithmes.isomap(1, 2, train_matrice_dissimilarite, test_matrice_dissimilarite)
+    algorithmes.pcoa(1, train_matrice_dissimilarite, test_matrice_dissimilarite)
+    algorithmes.hierarchique(5, train_matrice_dissimilarite, test_matrice_dissimilarite)
     algorithmes.knn(11, train_matrice_dissimilarite, test_matrice_dissimilarite, y_train, y_test)
